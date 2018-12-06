a8032a070d87578100fa784cfebd866f5160079e
Joketmac <526678993@qq.com>
2018-12-06 03:40:00 -0800

    首页

--- app/models.py
+++ app/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+# insert into axf_wheel(img,name,trackid) values("http://img01.bqstatic.com//upload/activity/2017031716035274.jpg@90Q.jpg","酸奶女王","21870"),("http://img01.bqstatic.com//upload/activity/2017031710450787.jpg@90Q.jpg","优选圣女果","21869"),("http://img01.bqstatic.com//upload/activity/2017030714522982.jpg@90Q.jpg","伊利酸奶大放价","21862"),("http://img01.bqstatic.com//upload/activity/2017032116081698.jpg@90Q.jpg","鲜货直供－窝夫小子","21770"),("http://img01.bqstatic.com//upload/activity/2017032117283348.jpg@90Q.jpg","鲜货直供－狼博森食品","21874");
+
+# 基础类
+class Base(models.Model):
+    img = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    trackid = models.CharField(max_length=8)
+
+    class Meta:
+        abstract = True
+
+
+# 轮播图 模型类
+class Wheel(Base):
+    class Meta:
+        db_table = 'axf_wheel'
+
+
+# 导航 模型类
+class Nav(Base):
+    class Meta:
+        db_table = 'axf_nav'
+
+
+
+# 每日必购 模型类
+class Mustbuy(Base):
+    class Meta:
+        db_table = 'axf_mustbuy'
+
+
+# 商品部分(分类) 模型类
+class Shop(Base):
+    class Meta:
+        db_table = 'axf_shop'
+
+# 商品列表
+class MainShow(models.Model):
+    trackid = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=100)
+    categoryid = models.CharField(max_length=20)
+    brandname = models.CharField(max_length=100)
+
+    img1 = models.CharField(max_length=100)
+    childcid1 = models.CharField(max_length=20)
+    productid1 = models.CharField(max_length=20)
+    longname1 = models.CharField(max_length=100)
+    price1 = models.CharField(max_length=10)
+    marketprice1 = models.CharField(max_length=10)
+
+    img2 = models.CharField(max_length=100)
+    childcid2 = models.CharField(max_length=20)
+    productid2 = models.CharField(max_length=20)
+    longname2 = models.CharField(max_length=100)
+    price2 = models.CharField(max_length=10)
+    marketprice2 = models.CharField(max_length=10)
+
+    img3 = models.CharField(max_length=100)
+    childcid3 = models.CharField(max_length=20)
+    productid3 = models.CharField(max_length=20)
+    longname3 = models.CharField(max_length=100)
+    price3 = models.CharField(max_length=10)
+    marketprice3 = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'axf_mainshow'
+
--- app/views.py
+++ app/views.py
@@ -2,5 +2,51 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from app.models import Wheel, Nav, Mustbuy, Shop, MainShow
+
+
 def home(request):
-    return render(request, 'home/home.html')
+
+    # 获取轮播图数据
+    wheels = Wheel.objects.all()
+
+    # 获取导航数据
+    navs = Nav.objects.all()
+
+    # 获取每日必购数据
+    mustbuys = Mustbuy.objects.all()
+
+    # 商品部分数据
+    shops = Shop.objects.all()  # 所有
+    shophead = shops[0]
+    shoptabs = shops[1:3]
+    shopclass = shops[3:7]
+    shopcommends = shops[7:11]
+
+    # 商品列表
+    mainShows = MainShow.objects.all()
+
+    data = {
+        'wheels':wheels,
+        'navs': navs,
+        'mustbuys': mustbuys,
+        'shophead': shophead,
+        'shoptabs': shoptabs,
+        'shopclass': shopclass,
+        'shopcommends': shopcommends,
+        'mainShows': mainShows
+    }
+
+    return render(request, 'home/home.html', context=data)
+
+
+def market(request):
+    return render(request, 'market/market.html')
+
+
+def cart(request):
+    return render(request, 'cart/cart.html')
+
+
+def mine(request):
+    return render(request, 'mine/mine.html')
